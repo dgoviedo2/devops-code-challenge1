81e293248c9037252136aee0cee291068ef52f62
Derek <Derek@MacBook-Air.local>
2026-03-17 19:46:22 -0400

    Add Terraform files for AWS infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "devops-challenge"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
