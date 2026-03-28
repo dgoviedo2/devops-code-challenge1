584970b36a98d3baf81c299d400024011bcf7101
Derek <Derek@MacBook-Air.local>
2026-03-28 17:55:17 -0400

    Updated config.js files with new ALB DNS

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  backendUrl: "http://devops-challenge-alb-1472515734.us-east-2.elb.amazonaws.com/api"
+  backendUrl: "http://devops-challenge-alb-1972959320.us-east-2.elb.amazonaws.com/api"
 };
 export default config;
